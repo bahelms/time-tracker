ea5ed95cfab3d074369e7c6b9f436aa6833f8cc0
Jimbonk Buchanon <jimbonkgit@gmail.com>
2023-10-06 10:40:28 -0400

    Store current session upon start command

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,7 @@
+use chrono::Utc;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -7,7 +11,7 @@ pub async fn start(location: String) {
         .await
         .expect("Failed to bind to addr");
 
-    println!("Time Tracker is running on {}", location);
+    println!("Time Tracker is now awaiting your command!");
 
     loop {
         let (stream, _addr) = listener.accept().await.unwrap();
@@ -32,14 +36,22 @@ fn execute_command(cmd: String) {
         ["START", session_id] => {
             start_session(session_id);
         }
-        _ => println!("Unknown command {}", cmd),
+        _ => {
+            println!("Unknown command {}", cmd);
+        }
     }
 }
 
-fn start_session(id: &str) {
-    println!("Starting session for {}", id);
+const SESSION_FILE: &str = "./current_session.txt";
 
-    // let time = Utc::now().to_rfc3339();
+fn start_session(id: &str) {
+    if Path::new(SESSION_FILE).exists() {
+        return println!("Can't start new session. A current session is still running.");
+    }
+
+    println!("Starting session for {}", id);
+    let session = format!("{}|{}", id, Utc::now().to_rfc3339());
+    store_session(session);
     // let parsed = chrono::DateTime::parse_from_rfc3339("2023-10-05T20:48:07.921875828+00:00");
     // match parsed {
     //     Ok(expr) => {
@@ -47,4 +59,24 @@ fn start_session(id: &str) {
     //     }
     //     Err(e) => println!("Error {}", e),
     // }
+}
+
+fn store_session(session: String) {
+    let mut file = File::create(SESSION_FILE).expect("Error creating file");
+    // let current_session_id = contents.split('|').collect::<Vec<&str>>()[0];
+    file.write_all(session.as_bytes())
+        .expect("Error writing session to file");
+}
+
+fn stop_session() {
+    let contents = read_file_contents(SESSION_FILE);
+    let end_time = Utc::now().to_rfc3339();
+}
+
+fn read_file_contents(pathname: &str) -> String {
+    let mut file = File::open(pathname).expect("Error opening file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Error reading session file");
+    contents
 }
